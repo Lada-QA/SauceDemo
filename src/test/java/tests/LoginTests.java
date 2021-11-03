c2d9f095540525fb3ccdcdf87b81e7a2c3ccf1fd
Ladauser <sovyhka29@gmail.com>
2021-11-03 13:32:35 +0300

    delete tests with AssertTrue(true), there are still tests to check messages(error).

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -5,23 +5,6 @@ import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest {
 
-    @Test
-    public void userNameLoginTest() {
-        loginPage.openPage("https://www.saucedemo.com/");
-        loginPage.setUsernameInput("standard_user");
-        loginPage.clickLoginButton();
-        loginPage.getLoginTitle();
-        Assert.assertTrue(true);
-    }
-
-    @Test
-    public void passwordLoginTest() {
-        loginPage.openPage("https://www.saucedemo.com/");
-        loginPage.setPasswordInput("secret_sauce");
-        loginPage.clickLoginButton();
-        loginPage.getLoginTitle();
-        Assert.assertTrue(true);
-    }
 
     @Test
     public void errorMessagePasswordLogin() {
